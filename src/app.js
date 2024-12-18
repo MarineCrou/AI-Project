fa620d4c17daf784f44cdc872e84c166acd5393c
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-12-18 11:51:41 +0100

    can't seem to fix option to hide the API key - so had to leave it inside js file.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,3 @@
-// API KEY
-let API_KEY;
-import { API_KEY as LocalAPIKey } from "./config.js";
-
-if (process.env.NODE_ENV === "production") {
-  API_KEY = window.API_KEY;
-} else {
-  API_KEY = LocalAPIKey;
-}
-
-console.log("Using API_KEY:", API_KEY);
-
 const renderMarkdown = (markdown) => {
   console.log(markdown);
   let markedDownContainer = document.getElementById("form-answer");
@@ -32,7 +20,7 @@ let getSearchValue = (e) => {
   console.log(inputValue);
 
   // -> Get the AI API link through Axios
-  const API_KEY = process.env.API_KEY || window.API_KEY;
+  let API_KEY = `tf486ac3343a3de0640fb9054f9boe8b`;
   let prompt = inputValue;
   let context = `"Provide a gluten-free and lactose-free version of the recipe requested by the user. Ensure that all ingredients are free from gluten and lactose, and suggest appropriate substitutions where necessary. The recipe must remain delicious, easy to follow, and true to the original dish’s flavors and textures. For example:
 If the user searches for 'tiramisu', replace gluten-containing ingredients like ladyfingers with gluten-free alternatives, and lactose-containing items like mascarpone with lactose-free substitutes.
